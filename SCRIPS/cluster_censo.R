11d7d204fb4c4fd5356f26ba46437224fbbbda5f
ariela <ariela.diaz@usach.cl>
2025-06-09 22:05:21 -0400

    cluster censo

--- SCRIPS/cluster_censo.R
+++ SCRIPS/cluster_censo.R
@@ -1,12 +1,14 @@
 # =============================================================================
 # 1) CARGAR LIBRERÍAS NECESARIAS
 # =============================================================================
+
 library(DBI)
 library(RPostgres)
 library(sf)
 library(ggplot2)
 library(cowplot)
-library(biscale)
+library(factoextra)
+library(ggfortify)
 
 # =============================================================================
 # 2) CONEXIÓN A BASE DE DATOS
@@ -58,13 +60,50 @@ ORDER BY ptje_esc_mayor_12 DESC;
 df_indicadores <- dbGetQuery(con, sql_indicadores)
 
 # =============================================================================
-# 4) CARGAR GEOMETRÍA DE ZONAS CENSALES
-# =============================================================================
-sql_geometria <- "
-SELECT
-  geocodigo::double precision AS geocodigo,
-  geom
-FROM dpa.zonas_censales_rm
-WHERE nom_provin = 'SANTIAGO' AND urbano = 1;
-"
-sf_zonas <- st_read(con, query = sql_geometria)
+# 4) Seleccionar variables y escalar 
+
+vars_clusters = df_indicadores[,c("ptje_migrantes", "ptje_esc_mayor_12", "ptje_adulto_mayor")]
+
+# se escalan las variables 
+vars_scaled = scale(vars_clusters)
+
+# 5) metodo del codo para elegir k 
+fviz_nbclust(vars_scaled, kmeans, method = "wss")
+  labs(tittle = "Metodo del codo", x = "Numero de clusteres (k)" , y = "wss")
+  
+# K-mean 
+set.seed(123)
+km = kmeans(vars_scaled, centers = 4, nstart = 25)
+
+df_indicadores$cluster = as.factor(km$cluster)
+
+# Escolaridad vs Migracion 
+# Ingreso vs Score
+ggplot(df_indicadores, aes(x = ptje_esc_mayor_12, y = ptje_migrantes, color = cluster)) +
+  geom_point(size = 2) +
+  labs(title = "Escolaridad v/s Migrantes",
+       x = "% poblacion con 12 años de escolaridad",
+       y = "% población migrante" ) +
+  theme_minimal()
+## Resultados
+  # C1 = escolaridad Media Baja, Pocos Migrantes, Pobacion joven  
+  # C2 = Escolaridad Media Alta, Alta Migracion, Población joven 
+  # C3 = Escolaridad Alta, Migración Media Baja, Población Mayor  
+  # C4 = Escolaridad Baja, Migracion Media Baja, Poblacion Mayor 
+
+if (!require("plotly")) install.packages("plotly", dependencies = TRUE)
+library(plotly)
+
+# Crear gráfico 3D
+fig <- plot_ly(
+  data = df_indicadores,
+  x = ~ptje_migrantes,
+  y = ~ptje_esc_mayor_12,
+  z = ~ptje_adulto_mayor,
+  color = ~as.factor(cluster),  # Asegurarse de que sea un factor para colorear por grupo
+  colors = c("red", "green", "blue", "purple"), # puedes ajustar esto
+  type = "scatter3d",
+  mode = "markers",
+  marker = list(size = 4)
+)
+
